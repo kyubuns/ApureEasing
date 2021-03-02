7ad45c6d95e039b38e4fb57c77f30de55c825e81
kyubuns <kyubuns@gmail.com>
2021-03-02 20:49:52 +0900

    feat: Parallel

---
+++ Assets/ApureEasing/Runtime/Parallel.cs
@@ -0,0 +1,53 @@
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace ApureEasing
+{
+    [UnitTitle("Parallel")]
+    [UnitCategory("ApureEasing")]
+    [TypeIcon(typeof(Sequence))]
+    public class ParallelNode : Unit
+    {
+        [Serialize]
+        private int count;
+
+        [DoNotSerialize]
+        [Inspectable]
+        [UnitHeaderInspectable("Count")]
+        public int Count
+        {
+            get => Mathf.Max(2, count);
+            set => count = Mathf.Max(value, 2);
+        }
+
+        [DoNotSerialize]
+        public ControlInput enter { get; private set; }
+
+        [DoNotSerialize]
+        public List<ControlOutput> next { get; private set; }
+
+        protected override void Definition()
+        {
+            enter = ControlInput(nameof(enter), RunCoroutine);
+
+            next = new List<ControlOutput>();
+            for (var i = 0; i < Count; i++)
+            {
+                var output = ControlOutput($"{i}");
+                next.Add(output);
+                Succession(enter, output);
+            }
+        }
+
+        private ControlOutput RunCoroutine(Flow flow)
+        {
+            var reference = flow.stack.ToReference();
+            foreach (var output in next)
+            {
+                Flow.New(reference).StartCoroutine(output);
+            }
+            return null;
+        }
+    }
+}
